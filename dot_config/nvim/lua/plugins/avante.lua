bba1260d5f061fbb5bcabecbc3169efc61d9b018
Jakob Külzer <jakob.kulzer@shopify.com>
2025-04-04 14:08:45 -0400

    Add .config/nvim/lua/plugins/avante.lua Add .config/nvim/lua/plugins/embark-theme.lua Add .config/nvim/lua/plugins/nordic.lua Add .config/nvim/lua/plugins/vim-color-spring-night.lua Add .config/nvim/lua/plugins/vim-vividchalk.lua

---
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,75 @@
+---@type LazySpec
+return {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    -- version = false,
+    config = function()
+        local openai = require "avante.providers.openai"
+        local has_openai_key = vim.fn.executable "openai_key" == 1
+
+        require("avante").setup {
+            -- @type AvanteProvider
+            provider = 'shopify-ai',         -- has_openai_key and "shopify-ai" or nil,
+            auto_suggestions_provider = nil, -- has_openai_key and "shopify-ai" or nil,
+            vendors = {
+                ["shopify-ai"] = {
+                    __inherited_from = 'openai',
+                    endpoint = "https://proxy.shopify.ai/v3/v1",
+                    model = "anthropic:claude-3-5-sonnet-v2",
+                    api_key_name = "cmd:openai_key cat",
+                    parse_curl_args = openai.parse_curl_args,
+                    -- parse_response_data = openai.parse_response,
+                },
+            },
+            hints = { enabled = true },
+            highlights = {
+                diff = {
+                    incoming = "DiffChange",
+                },
+            },
+            mappings = {
+                suggestion = {
+                    next = "]]",
+                    prev = "[[",
+                },
+            },
+        }
+    end,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua",      -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
+    },
+}
---
+++ dot_config/nvim/lua/plugins/vim-color-spring-night.lua
@@ -0,0 +1,3 @@
+return {
+    "rhysd/vim-color-spring-night"
+}
