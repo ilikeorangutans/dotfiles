09c364852dc30d1519c0e5fac63549099e823168
Jakob Külzer <jakob.kulzer@shopify.com>
2025-04-07 09:19:53 -0400

    Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,19 +1,16 @@
 return {
-    {
-        "junegunn/fzf.vim",
-        build = "./install --all",
-        keys = {
-            { "ff", "<cmd>Files<cr>",   mode = "n" },
-            { "ft", "<cmd>Tags<cr>",    mode = "n" },
-            { "fT", "<cmd>BTags<cr>",   mode = "n" },
-            { "fb", "<cmd>Buffers<cr>", mode = "n" },
-            { "fc", "<cmd>Rg<cr>",      mode = "n" },
-            { "fl", "<cmd>BLines<cr>",  mode = "n" },
-            { "fh", "<cmd>History<cr>", mode = "n" },
-        },
-    },
-    "junegunn/fzf",
-    init = function()
-        vim.g['$FZF_DEFAULT_COMMAND'] = 'fd --type f --color=always' --exclude .git --ignore-file ~/.gitignore'
-    end
+	{
+		"junegunn/fzf.vim",
+		build = "./install --all",
+		keys = {
+			{ "ff", "<cmd>Files<cr>", mode = "n" },
+			{ "ft", "<cmd>Tags<cr>", mode = "n" },
+			{ "fT", "<cmd>BTags<cr>", mode = "n" },
+			{ "fb", "<cmd>Buffers<cr>", mode = "n" },
+			{ "fc", "<cmd>Rg<cr>", mode = "n" },
+			{ "fl", "<cmd>BLines<cr>", mode = "n" },
+			{ "fh", "<cmd>History<cr>", mode = "n" },
+		},
+	},
+	"junegunn/fzf",
 }
