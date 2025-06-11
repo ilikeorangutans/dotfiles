585f2b7ffea39aa6105ab384bd268cdd61039111
Jakob Külzer <jakob.kulzer@shopify.com>
2025-06-11 13:40:14 -0400

    Update .config/nvim/lua/plugins/nvim-cmp.lua

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,57 +1,57 @@
 local config = function()
-  local cmp = require("cmp")
+    local cmp = require("cmp")
 
-  cmp.setup({
-      snippet = {
-          expand = function(args)
-              vim.fn["UltiSnips#Anon"](args.body)
-          end,
-      },
-      window = {},
-      mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                  cmp.select_next_item()
-              elseif has_words_before() then
-                  cmp.complete()
-              else
-                  fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-              end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function()
-              if cmp.visible() then
-                  cmp.select_prev_item()
-              elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                  feedkey("<Plug>(vsnip-jump-prev)", "")
-              end
-          end, { "i", "s" }),
-      }),
-      sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'ultisnips' },
-          { name = 'buffer' },
-      }),
-  })
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                vim.fn["UltiSnips#Anon"](args.body)
+            end,
+        },
+        window = {},
+        mapping = cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                end
+            end, { "i", "s" }),
+            ["<S-Tab>"] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    feedkey("<Plug>(vsnip-jump-prev)", "")
+                end
+            end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'ultisnips' },
+            { name = 'buffer' },
+        }),
+    })
 
-  vim.g.UltiSnipsJumpForwardTrigger = '<C-3>'
-  vim.g.UltiSnipsJumpBackwardTrigger = '<C-4>'
+    vim.g.UltiSnipsJumpForwardTrigger = '<C-3>'
+    vim.g.UltiSnipsJumpBackwardTrigger = '<C-4>'
 end
 
 return {
     'hrsh7th/nvim-cmp',
-  config = config,
-  event = 'InsertEnter',
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'SirVer/ultisnips',
-    'quangnguyen30192/cmp-nvim-ultisnips',
-    'honza/vim-snippets',
-  },
+    config = config,
+    event = 'InsertEnter',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'SirVer/ultisnips',
+        'quangnguyen30192/cmp-nvim-ultisnips',
+        'honza/vim-snippets',
+    },
 }
