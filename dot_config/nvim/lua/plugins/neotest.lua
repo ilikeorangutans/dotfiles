ce024ad97eb5c944b906b51bb3907f1dadb98f59
Jakob Külzer <jakob.kulzer@shopify.com>
2026-02-10 14:39:58 -0500

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -17,7 +17,7 @@ return {
         "nvim-lua/plenary.nvim",
         "antoinemadec/FixCursorHold.nvim",
         { "nvim-treesitter/nvim-treesitter", branch = "main" },
-        { "fredrikaverpil/neotest-golang", version = "*" },
+        { "fredrikaverpil/neotest-golang",   version = "*" },
         "lawrence-laz/neotest-zig",
         "zidhuss/neotest-minitest",
     },
