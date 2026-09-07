32ba097d1598cce35f808ffbb32d54870cefc790
Jakob Külzer <jakob.kuelzer@gmail.com>
2026-09-06 22:45:22 -0400

    added emmet

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -911,6 +911,9 @@ require("lazy").setup({
 		ft = { "go", "gomod" },
 		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
+  {
+    "mattn/emmet-vim",
+  },
 	{
 		"nvim-neotest/neotest",
 		dependencies = {
